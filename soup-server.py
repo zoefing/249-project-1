d3683e79501f8c0cd1e511a9ecb429049bfadfe3
Zoe Fingleton <141530755+zoefing@users.noreply.github.com>
2024-09-20 23:23:11 -0400

    fix intro

--- soup-server.py
+++ soup-server.py
@@ -3,8 +3,8 @@
 import socket
 import sys
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+HOST = "127.0.0.1"  # This is the loopback address
+PORT = 65432        # The port used by the server
 
 grocery_aisles = {
     "1": "produce",
